547e1d70b491beb970b5b27ed39b030aad787ef3
Datoh <datoh44@gmail.com>
2024-04-27 18:59:58 +0200

    Add player moves

---
+++ player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody3D
+
+const SPEED = 20.0
+const Z_MIN = -10.0
+const Z_MAX = 10.0
+const Y_MIN = 2.0
+const Y_MAX = 10.0
+
+func _physics_process(delta):
+  var input_dir = Input.get_vector("ui_down", "ui_up", "ui_left", "ui_right")
+  var direction = (transform.basis * Vector3(0, input_dir.x, input_dir.y)).normalized()
+  if direction:
+    velocity.y = direction.y * SPEED
+    velocity.z = direction.z * SPEED
+  else:
+    velocity.y = move_toward(velocity.y, 0, SPEED)
+    velocity.z = move_toward(velocity.z, 0, SPEED)
+
+  move_and_slide()
+  position.y = clamp(position.y, Y_MIN, Y_MAX)
+  position.z = clamp(position.z, Z_MIN, Z_MAX)
